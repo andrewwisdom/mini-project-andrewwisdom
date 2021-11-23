0261fbe9bf59b3b7b924f7439cc6cc350558c456
Andrew Wisdom <andrew.L.wisdom@hotmail.com>
2021-11-23 09:23:25 -0600

    not yet working put no eclipse error

--- src/main/java/com/revature/app/Collection.java
+++ src/main/java/com/revature/app/Collection.java
@@ -1,5 +1,5 @@
 package com.revature.app;
 
 public class Collection {
-	
+	String[] collection;
 }
--- src/main/java/com/revature/app/Driver.java
+++ src/main/java/com/revature/app/Driver.java
@@ -1,5 +1,30 @@
 package com.revature.app;
 
-public class Driver {
+import io.javalin.Javalin;
 
+public class Driver {
+	static int currentIndex;
+	static Collection coll = new Collection();
+	
+	public static void main(String[] args) {
+		currentIndex = 0;
+		
+		Javalin app = Javalin.create();
+		
+		app.start(7777);
+		 
+		app.post("/collection", ctx -> {
+			coll.collection[currentIndex] = ctx.formParam("item");
+			currentIndex++;
+			
+			String responseText = "";
+			
+			for (int index = 0; index < currentIndex; index++) {
+				if (coll.collection[index] != null) {
+					responseText += coll.collection[index] + "<br>";
+				}
+			}
+			ctx.html(responseText);
+		});
+	}
 }
